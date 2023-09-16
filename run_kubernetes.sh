136568a4d74badfd802fece14acb27753587cfb6
Nguyễn Hoàng Duy <32288295+nhdlklk@users.noreply.github.com>
2023-09-16 17:03:06 +0700

    add output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="nhdlklk/microproject:latest"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microproject\
+    --image=$dockerpath\
+    --port=80 --labels app=microproject
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microproject 8000:80
